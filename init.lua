d5326b8d08ba00ed819b6a0db1093362c5430c6b
jack sharville <jacksharville@gmail.com>
2025-02-21 11:54:25 +0000

    Renamed lsp update

--- init.lua
+++ init.lua
@@ -361,6 +361,11 @@ require("lazy").setup({
 	},
 
 	-- LSP Plugins
+	-- {
+	-- 	"pmizio/typescript-tools.nvim",
+	-- 	dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+	-- 	opts = {},
+	-- },
 	{
 		-- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
 		-- used for completion, annotations and signatures of Neovim apis
@@ -553,8 +558,7 @@ require("lazy").setup({
 				--    https://github.com/pmizio/typescript-tools.nvim
 				--
 				-- But for many setups, the LSP (`ts_ls`) will work just fine
-				-- ts_ls = {},
-				tsserver = {},
+				ts_ls = {},
 				--
 
 				lua_ls = {
